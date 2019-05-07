c90a2335ac8047b2d728dd3e623e92262ae8e4d8
Lpu8er <lpu8er@gmail.com>
2019-05-07 18:29:52 +0200

    leaderboard command

---
+++ src/Util/DiscordCommand/DiscordLeaderboardCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Util\DiscordCommand;
+
+use App\Service\Discord;
+
+/**
+ * Description of DiscordLeaderboardCommand
+ *
+ * @author lpu8er
+ */
+class DiscordLeaderboardCommand extends DiscordCommand {
+    const SIZE = 5;
+    
+    public function help(Discord $discordService) {
+        $discordService->talk('`.leaderboard` display the money leaderboard', $this->data['channel_id']);
+    }
+    
+    public function execute(Discord $discordService) {
+        $returns = [];
+        $dsl = $discordService->getEntityManager()->getRepository(\App\Entity\DiscordUser::class)->findAll();
+        foreach($dsl as $ds) {
+            $returns[$ds->getDiscordName().'#'.$ds->getDiscriminator()] = $ds->getMoney();
+        }
+        arsort($returns);
+        $returns = array_slice($returns, 0, static::SIZE);
+        $msg = 'Money leaderboard : ';
+        foreach($returns as $un => $mn) {
+            $msg .= PHP_EOL.'**'.$un.'** '.number_format($mn, 2).' :euro: ';
+        }
+        $discordService->talk($msg, $this->data['channel_id']);
+    }
+}
