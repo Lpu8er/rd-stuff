71a18b67d1406b69ec165b89ff99ae4070a77abb
Lpu8er <lpu8er@gmail.com>
2019-06-01 13:09:57 +0200

    Cleanup on restart

--- src/Command/KillDiscord.php
+++ src/Command/KillDiscord.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Command;
 
+use App\Entity\Hangman;
 use App\Entity\MessageQueue;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
@@ -28,6 +29,15 @@ class KillDiscord extends Command {
     protected function execute(InputInterface $input, OutputInterface $output) {
         $output->write('Killing');
         $this->em->getRepository(MessageQueue::class)->register('kill');
+        $this->cleanup();
         $output->writeln(' killed.');
+    }
+    
+    protected function cleanup() {
+        $hs = $this->em->getRepository(Hangman::class)->findAll();
+        foreach($hs as $h) {
+            $this->em->persist($h);
+            $this->em->flush();
+        }
     }
 }
