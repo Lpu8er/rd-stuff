46659f14b2e892450ae06ec7d1190290bc57efd0
Lpu8er <lpu8er@gmail.com>
2019-06-18 13:36:03 +0200

    Add dl route

--- src/Controller/GrysController.php
+++ src/Controller/GrysController.php
@@ -19,4 +19,19 @@ class GrysController extends Controller {
     public function home(Request $request, \Psr\Log\LoggerInterface $logger) {
         return $this->render('grys.html.twig');
     }
+    
+    /**
+     * @Route("/dl/{f}", name="grys_dl", host="grys.ovh")
+     * 
+     */
+    public function dl(Request $request, \Psr\Log\LoggerInterface $logger, $f) {
+        $bp = $this->getParameter('dir.downloads');
+        if(!empty($bp) && !empty($f) && preg_match('`^([a-zA-Z0-9_-]+)\.([a-z]+)$`iU', $f)) {
+            $path = $bp.'/'.$f;
+            $returns = $this->file($path);
+        } else {
+            $retuns = $this->home($request, $logger);
+        }
+        return $returns;
+    }
 }
