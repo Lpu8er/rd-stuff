ec10c3ef30fe3005bfe5326dcdced00d3de669ea
Lpu8er <lpu8er@gmail.com>
2019-06-18 13:24:30 +0200

    Fix rd route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class HomeController extends Controller {
     /**
-     * @Route("/", name="rd_default", host="lpu8er.net")
+     * @Route("/", name="rd_default", host="rd.lpu8er.net")
+     * 
+     */
+    public function rdhome(Request $request, \Psr\Log\LoggerInterface $logger) {
+        return $this->render('home.html.twig');
+    }
+    
+    /**
+     * @Route("/", name="base_default", host="lpu8er.net")
      * 
      */
     public function home(Request $request, \Psr\Log\LoggerInterface $logger) {
