e10cfbeda5681f4f084272e7e7d409089d2c4119
Lpu8er <lpu8er@gmail.com>
2019-06-18 13:52:02 +0200

    Add debug

--- src/Controller/GrysController.php
+++ src/Controller/GrysController.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,7 +18,7 @@ class GrysController extends Controller {
      * @Route("/", name="grys_default", host="grys.ovh")
      * 
      */
-    public function home(Request $request, \Psr\Log\LoggerInterface $logger) {
+    public function home(Request $request, LoggerInterface $logger) {
         return $this->render('grys.html.twig');
     }
     
@@ -24,11 +26,16 @@ class GrysController extends Controller {
      * @Route("/dl/{f}", name="grys_dl", host="grys.ovh")
      * 
      */
-    public function dl(Request $request, \Psr\Log\LoggerInterface $logger, $f) {
+    public function dl(Request $request, LoggerInterface $logger, $f) {
         $bp = $this->getParameter('dir.downloads');
         if(!empty($bp) && !empty($f) && preg_match('`^([a-zA-Z0-9_-]+)\.([a-z]+)$`iU', $f)) {
             $path = $bp.'/'.$f;
-            $returns = $this->file($path);
+            $ff = new File($path);
+            if($ff->isFile()) {
+                $returns = $this->file($path);
+            } else {
+                $returns = $this->createNotFoundException('File "'.$path.'" not found');
+            }
         } else {
             $returns = $this->home($request, $logger);
         }
